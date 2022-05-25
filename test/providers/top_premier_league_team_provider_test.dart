51143fa810257386054b3bfae24fbf9488b71bbe
Kiyrean Dyer-Allen <kda_100@yahoo.co.uk>
2022-05-25 15:51:06 +0100

    updated provider test

--- test/providers/top_premier_league_team_provider_test.dart
+++ test/providers/top_premier_league_team_provider_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -7,7 +5,6 @@ import 'package:top_football_team/models/api_response.dart';
 import 'package:top_football_team/models/team.dart';
 import 'package:top_football_team/providers/top_premier_league_team_provider.dart';
 import 'package:top_football_team/repositories/premier_league_repository.dart';
-import 'package:mockito/mockito.dart';
 import '../helpers_for_tests/dummy_data/dummy_team_object.dart';
 import '../helpers_for_tests/mocks/premier_league_repository_mock.dart';
 
@@ -28,26 +25,30 @@ void main() {
   group("TopPremierLeagueTeamProvider function tests", () {
     late TopPremierLeagueTeamProvider topPremierLeagueTeamProvider;
     late MockPremierLeagueRepository mockPremierLeagueRepository;
+    late bool listenersNotified;
 
     /// sets up provider object and mock repository, then assign repository to provider,
     /// so functionality can be mocked.
     setUp(() {
+      listenersNotified = false;
       topPremierLeagueTeamProvider = TopPremierLeagueTeamProvider();
       mockPremierLeagueRepository = MockPremierLeagueRepository();
       topPremierLeagueTeamProvider.premierLeagueRepository =
           mockPremierLeagueRepository;
+      topPremierLeagueTeamProvider.addListener(() => listenersNotified = true);
+    });
+
+    tearDown(() {
+      topPremierLeagueTeamProvider.dispose();
     });
 
     test(
         "When setTopPremierLeagueTeam sets a team object the ApiResponse object becomes a success and listeners are notified",
         () async {
       final Team team = DummyTeamObject.genTeam();
-      bool listenersNotified = false;
-      topPremierLeagueTeamProvider.addListener(() => listenersNotified = true);
       when(mockPremierLeagueRepository.getTopPremierLeagueTeam())
           .thenAnswer((_) async => team);
       await topPremierLeagueTeamProvider.setTopPremierLeagueTeam();
-
       expect(listenersNotified, true);
       expect(topPremierLeagueTeamProvider.apiResponse.responseType,
           ApiResponseType.SUCCESS);
@@ -57,12 +58,9 @@ void main() {
     test(
         "When setTopPremierLeagueTeam sets a null object for team the ApiResponse object becomes a success and listeners are notified",
         () async {
-      bool listenersNotified = false;
-      topPremierLeagueTeamProvider.addListener(() => listenersNotified = true);
       when(mockPremierLeagueRepository.getTopPremierLeagueTeam())
           .thenAnswer((_) async => null);
       await topPremierLeagueTeamProvider.setTopPremierLeagueTeam();
-
       expect(listenersNotified, true);
       expect(topPremierLeagueTeamProvider.apiResponse.responseType,
           ApiResponseType.SUCCESS);
@@ -72,12 +70,9 @@ void main() {
     test(
         "When setTopPremierLeagueTeam throws an error the ApiResponse object becomes a error and listeners are notified",
         () async {
-      bool listenersNotified = false;
-      topPremierLeagueTeamProvider.addListener(() => listenersNotified = true);
       when(mockPremierLeagueRepository.getTopPremierLeagueTeam())
           .thenAnswer((_) => throw Exception());
       await topPremierLeagueTeamProvider.setTopPremierLeagueTeam();
-
       expect(listenersNotified, true);
       expect(topPremierLeagueTeamProvider.apiResponse.responseType,
           ApiResponseType.ERROR);
